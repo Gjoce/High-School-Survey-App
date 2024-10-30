0f684b8621695ad61e748d1493e726d9e5a5c337
Georgi Dimov <gjorgji.dimov@student.um.si>
2024-10-30 13:46:44 +0100

    try4

--- BACKEND/config/database.js
+++ BACKEND/config/database.js
@@ -3,10 +3,13 @@ const knex = require("knex");
 const db = knex({
   client: "mysql2",
   connection: process.env.JAWS_URL,
-  pool: {
-    min: 0,
-    max: 10,
-  },
 });
 
-module.exports = db;
+db.raw("SELECT 1")
+  .then(() => {
+    console.log("Database connection successful!");
+    db.destroy();
+  })
+  .catch((err) => {
+    console.error("Database connection failed:", err);
+  });
