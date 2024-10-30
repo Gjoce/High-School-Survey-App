4bc9c2afda854bb012cfd77cba7c87793cea920b
Georgi Dimov <gjorgji.dimov@student.um.si>
2024-10-30 14:18:17 +0100

    try10

--- BACKEND/config/database.js
+++ BACKEND/config/database.js
@@ -3,7 +3,10 @@ const knex = require("knex");
 const db = knex({
   client: "mysql2",
   connection: {
-    connectionString: process.env.JAWSDB_URL,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     ssl: { rejectUnauthorized: false },
   },
   pool: { min: 0, max: 5 },
