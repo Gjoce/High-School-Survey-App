d863db2af7125ee37911794891d52b805e1fe6dd
Georgi Dimov <gjorgji.dimov@student.um.si>
2024-10-31 14:37:29 +0100

    db

--- BACKEND/config/database.js
+++ BACKEND/config/database.js
@@ -9,7 +9,7 @@ const db = knex({
     database: process.env.DB_DATABASE,
     ssl: { rejectUnauthorized: false },
   },
-  pool: { min: 0, max: 35 },
+  pool: { min: 0, max: 50 },
 });
 
 module.exports = db;
